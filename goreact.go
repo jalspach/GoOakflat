6024f1d5c15e6bd5e3cbc36d3714ef89f23a5fed
James Alspach <jalspach@gmail.com>
2020-06-21 19:15:31 -0700

    Fignting with 2 runtime errors ./goreact.go:79:2: syntax error: unexpected func, expecting field name or embedded type ./goreact.go:82:1: syntax error: non-declaration statement outside function body

--- goreact.go
+++ goreact.go
@@ -1,15 +1,73 @@
 package main // Basic goReact program
 
-import "fmt" //"/Users/jalspach/Documents/GitHub/simparts/parts/parts.go"
+import (
+	"fmt"
+	//"parts"
+)
 
-//"github.com/jalspach/simparts.git/parts.go"
-//"/simparts/parts.go"
-//"github.com/jalspach/simparts/parts.go"
+//New structs
+
+// BasicStats contains all of the stats common to every part
+type BasicStats struct {
+	RAD      int // radiation absorbed dose (100 RAD = 1 Gy (gray))
+	DegC     int // Temp in deg C
+	Bar      int // Pressure in bar
+	Vol      int // Current volume in cu m
+	AgeHours int // Number of hours the part has been in opperation
+
+	// Limits
+	LimVolume   int // LimVolume - Amount of collant max
+	LimRAD      int // LimRAD - Number of survivable RAD's
+	LimDegC     int // LimDegC - Max temprature
+	LimBar      int // LimBar - Max Pressure
+	LimAgeHours int // LimAgeHours - Max Age
+
+	// Status
+	Damage     int    // Damage is 0-100 to denote percentage of damage to the part (100 is total fail)
+	AlarmColor string // Red or Yellow alarm indicator
+}
+
+type BasicStatic struct {
+	BasicStats // basic stats
+}
+
+// BasicDynamic adds dynamic stats to static parts
+type BasicDynamic struct {
+	BasicStats     // basic stats
+	RPM        int // Revs per min
+	KWH        int // (KWH produced (positive) or consumed (negitive))
+	ReqUse     int // Requested Use in % of the max
+	CurUse     int // Current Use in % of max
+
+	// Limits
+	LimKWH int // LimKwH
+	LimRPM int // LimRPM
+}
+
+//Interface
+type Curstatus interface {
+	Alarm() string
+	Values() string
+}
+
+//Methods
+func (b BasicDynamic) Alarm() string {
+	switch {
+	case b.Damage >= 75:
+		return "Red Alarm"
+	case b.Damage >= 50:
+		return "Yellow Alarm"
+	case b.Damage >= 25:
+		return "C&W Alarm"
+	default:
+		return "no alarm"
+	}
+}
 
 //start by defining two parts and giving them qualities. Pass data beteen them.
 func main() {
 	PCP := BasicDynamic{
-		BasicStats: BasicStats{0, 0, 0, 0, 100, 100, 100, 100, 100, 0, "Green"},
+		BasicStats: BasicStats{0, 0, 0, 0, 0, 100, 100, 100, 100, 100, 0, "Green"},
 		RPM:        0,
 		KWH:        0,
 		ReqUse:     0,
@@ -21,62 +79,5 @@ func main() {
 	fmt.Println("AlarmColor", PCP.AlarmColor)
 	fmt.Println("RPM", PCP.RPM)
 	fmt.Println(PCP)
-}
-
-//New structs
-// BasicStats contains all of the stats common to every part
-type BasicStats struct {
-	// radiation absorbed dose (100 RAD = 1 Gy (gray))
-	RAD int
-	// Temp in deg C
-	DegC int
-	// Pressure in bar
-	Bar int
-	// Number of hours the part has been in opperation
-	AgeHours int
-
-	// Limits
-	// LimVolume - Amount of collant max
-	LimVolume int
-	// LimRAD - Number of survivable RAD's
-	LimRAD int
-	// LimDegC - Max temprature
-	LimDegC int
-	// LimBar - Max Pressure
-	LimBar int
-	// LimAgeHours - Max Age
-	LimAgeHours int
-
-	// Status
-	// Damage is 0-100 to denote percentage of damage to the part (100 is total fail)
-	Damage int
-	// Red or Yellow alarm indicator
-	AlarmColor string
-}
-
-type BasicStatic struct {
-	// basic stats
-	BasicStats
-}
-type BasicDynamic struct {
-	// basic stats
-	BasicStats
-	// Revalutions per min (this can either be passive like a turbine or active like a pump)
-	RPM int
-	// (KWH produced (positive) or consumed (negitive))
-	KWH int
-	// Requested Use in % of the max
-	ReqUse int
-	// Current Use in % of max
-	CurUse int
-	// Limits
-	// LimKwH
-	LimKWH int
-	// LimRPM
-	LimRPM int
-
-	//Functions
-	func (p BasicDynamic) Alarm() bool {
-		return r.Damage > 50
-	}
+	fmt.Println("PCP Status is", PCP.Alarm())
 }
